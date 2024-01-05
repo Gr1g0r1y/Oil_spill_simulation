e5c94b23e976acddfc6bd824fe6be027b9db6318
Gr1g0r1y <155245888+Gr1g0r1y@users.noreply.github.com>
2024-01-05 18:55:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import sys
 import math
 from random import randint
 
-
 import pygame as pg
 from pygame.locals import *
 
@@ -10,39 +9,36 @@ import colors as color
 import config as cfg
 from objects import Fish, InputBox, Oil
 
-
-
-
 if __name__ == "__main__":
     pg.init()
-
+    
     fpsClock = pg.time.Clock()
     screen = pg.display.set_mode((cfg.WIDTH, cfg.HEIGHT))
-
+    
     startProcess = False
     fishKelledCounter = 0
     startTime = 0
-
+    
     fishes = pg.sprite.Group()
     for _ in range(cfg.FISH_COUNT):
         f = Fish(randint(0, cfg.WIDTH), randint(0, cfg.HEIGHT - 100))
         fishes.add(f)
     
     img = pg.image.load("img.png")
-
+    
     input_box_Q = InputBox(220, 540, 100, 32)
     input_box_d_p = InputBox(220, 610, 100, 32)
     input_box_q = InputBox(455, 540, 100, 32)
     input_box_mu = InputBox(455, 610, 100, 32)
     input_box_V = InputBox(670, 540, 100, 32)
     input_box_Z = InputBox(670, 610, 100, 32)
-
+    
     input_boxes = [
-        input_box_d_p, input_box_Q, 
-        input_box_mu, input_box_q, 
+        input_box_d_p, input_box_Q,
+        input_box_mu, input_box_q,
         input_box_V, input_box_Z
     ]
-
+    
     scoreFont = pg.font.SysFont("monospace", 15)
     f_p = pg.font.SysFont('arial', 48)
     text_Q = f_p.render("Q =", 2, (0, 0, 0))
@@ -53,7 +49,6 @@ if __name__ == "__main__":
     text_zalupa = f_p.render("x =", 2, (0, 0, 0))
     text_s = f_p.render("S = 0", 2, (0, 0, 0))
     text_h = pg.font.SysFont('arial', 40).render("H = 0 см", 2, (0, 0, 0))
-    
     
     while True:
         screen.fill(color.ICE_BLUE)
@@ -68,16 +63,16 @@ if __name__ == "__main__":
                 ms_x, ms_y = pg.mouse.get_pos()
                 if 20 <= ms_x <= 120 and 620 <= ms_y <= 670:
                     oil = Oil(d_p=float(input_box_d_p.text), q=float(input_box_q.text), mu_0=float(input_box_mu.text),
-                            Q=float(input_box_Q.text), V=float(input_box_V.text))
+                              Q=float(input_box_Q.text), V=float(input_box_V.text), zalupa=float(input_box_Z.text))
                     startProcess = True
                     oil.run(pg.time.get_ticks())
-                    
+            
             for box in input_boxes:
-                box.handle_event(event)        
+                box.handle_event(event)
         
         if startProcess:
             oil.draw(screen, pg.time.get_ticks())
-            fishes.update(oil.currentRadius)
+            fishes.update(oil.currentRadius / oil.zalupa)
             text_time = pg.font.SysFont('arial', 40).render(f"t = {oil.time // 100 / 10}s", 2, (0, 0, 0))
             text_r = pg.font.SysFont('arial', 40).render(f"R = {oil.getNewRadius(pg.time.get_ticks())} м", 2, (0, 0, 0))
             text_s = pg.font.SysFont('arial', 40).render(f"S = {round(oil.get_square())} м^2", 2, (0, 0, 0))
@@ -86,31 +81,26 @@ if __name__ == "__main__":
             fishes.update(0)
             text_time = pg.font.SysFont('arial', 40).render("t = 0s", 2, (0, 0, 0))
             text_r = pg.font.SysFont('arial', 40).render("R = 0 м", 2, (0, 0, 0))
-          
-          
+        
         # fishes
         fishes.draw(screen)
-         
+        
         # panel
         pg.draw.rect(screen, color.PANEL, (0, 500, cfg.WIDTH, 200))
         pg.draw.rect(screen, color.START_BUTTON, (20, 620, 100, 50))
         
-    
         for f in fishes:
             fishKelledCounter += f.getPoint()
-            
+        
         for box in input_boxes:
             box.update()
             box.draw(screen)
-            
-            
-       
+        
         screen.blit(text_time, (800, 500))
         screen.blit(text_r, (800, 550))
         screen.blit(text_s, (800, 600))
         screen.blit(text_h, (800, 650))
         label = scoreFont.render(f"Fishs alive: {cfg.FISH_COUNT - fishKelledCounter}", 2, (255, 0, 0))
-        
         
         # Отрисовка текста на основном окне игры
         screen.blit(label, (10, 10))
